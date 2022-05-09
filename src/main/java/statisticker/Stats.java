e8bdf9c294cfc158913fe4c834d304d2b8beeb8a
vbiradar <vikhyat.biradar@gmail.com>
2022-05-09 12:28:24 +0530

    added files and implementation

---
+++ src/main/java/statisticker/Stats.java
@@ -0,0 +1,33 @@
+package statisticker;
+
+public class Stats {
+
+	private float min;
+	private float max;
+	private float average;
+
+	public float getMin() {
+		return min;
+	}
+
+	public void setMin(float min) {
+		this.min = min;
+	}
+
+	public float getMax() {
+		return max;
+	}
+
+	public void setMax(float max) {
+		this.max = max;
+	}
+
+	public float getAverage() {
+		return average;
+	}
+
+	public void setAverage(float average) {
+		this.average = average;
+	}
+
+}
